246127dd04985a0239ff701c1e5d3016ac81836a
AbbasHamid <71654722+AbbasHamid@users.noreply.github.com>
2021-12-16 09:34:28 +0100

    Login + registrera
    Möjligt att registrera sig med samma namn + efternamn. Dock olika personnummer och email.
    
    Ändra  funktionen för login så att meddelanden vid fel inmatning visas rätt.
    
    Andra små fix

--- stada_ab/pages/api/users/index.js
+++ stada_ab/pages/api/users/index.js
@@ -20,15 +20,6 @@ export default async (req, res) => {
       try {
         const user = await User.create(req.body);
 
-        /* const client = await MongoClient.connect(
-          "mongodb+srv://Abbas:1111@cluster0.mdcz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-        );
-        const db = client.db();
-
-        const bookingCollection = db.collection("bookings");
-        const result = await bookingCollection.insertOne(data);
-        client.close();
- */
         res.status(201).json({ success: true, data: user });
       } catch (error) {
         res.status(400).json({ success: false });
