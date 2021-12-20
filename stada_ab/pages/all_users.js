364ec85f008d35b6f853b0d6fa67f7a2b0692a7f
Keici232 <71629430+Keici232@users.noreply.github.com>
2021-12-20 10:34:14 +0100

    visa alla users + delar upp kunder och städare

--- stada_ab/pages/all_users.js
+++ stada_ab/pages/all_users.js
@@ -1,27 +1,45 @@
 import UserCard from "../components/user-card";
 
-function all_users() {
-    return (
-        <div className="page-body user-card mt-5 mb-5">
-            {/*Iterera alla städare här*/}
-            <div>
-                <h4 className="mb-5">Alla städarna</h4>
-                <div>
-                    <UserCard />
-                    <hr />
-                </div>
-            </div>
+export default function all_users({ data }) {
 
-            {/*Iterera alla vanliga användare/kunder */}
-            <div>
-                <h4 className="mb-5">Alla kunder</h4>
-                <div>
-                    <UserCard />
-                    <hr />
-                </div>
-            </div>
-        </div >
-    );
+  return (
+    <div className="page-body user-card mt-5 mb-5">
+      <div>
+        <h4 className="mb-5">Alla städarna</h4>
+        <div>
+          {data.map((user) => {
+              if(user.role == "Cleaner"){
+            return <UserCard user={user} />
+              }
+          })}
+          <hr />
+        </div>
+      </div>
+
+      <div>
+        <h4 className="mb-5">Alla kunder</h4>
+        <div>
+        {data.map((user) => {
+              if(user.role == "Customer"){
+            return <UserCard user={user} />
+              }
+          })}
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default all_users;
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/users");
+
+  const { data } = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
